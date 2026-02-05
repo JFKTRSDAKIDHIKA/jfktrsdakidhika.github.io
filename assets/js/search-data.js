12bca933c18879c826c6722507903ca48ab0b9b0
JFKTRSDAKIDHIKA <2023210131@bupt.cn>
2026-02-05 13:54:17 +0000

    Deploying to gh-pages from @ JFKTRSDAKIDHIKA/jfktrsdakidhika.github.io@73ac63dff489668bc05158d7c44f8fef93784333 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-golden-apple-corelet",
+          title: 'Golden Apple Corelet',
+          description: "A compact, in-order RISC-V microarchitecture optimized for embedded systems.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
